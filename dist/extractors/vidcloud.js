2169935d3b74d256dd86eea392f779c1ff04c1b6
streamamos <134351107+streamamos@users.noreply.github.com>
2024-05-03 01:35:24 +0100

    Update vidcloud.js (vidcloud sv working)

--- dist/extractors/vidcloud.js
+++ dist/extractors/vidcloud.js
@@ -86,7 +86,13 @@ class VidCloud extends models_1.VideoExtractor {
            
                 return result; */
                 res = await this.client.post("https://rabbitthunder-ruddy.vercel.app/api/upcloud", { id: id });
-                const { source, subtitle } = res.data;
+                let { source, subtitle } = res.data;
+                if (source.startsWith("https://b-g-")) {
+                    const parts = source.split('/');
+                    if (parts.length >= 4) {
+                        source = `https://ek.megacdn.co:2228/${parts.slice(3).join('/')}`;
+                    }
+                }
                 const result = {
                     sources: [{
                             url: source,
